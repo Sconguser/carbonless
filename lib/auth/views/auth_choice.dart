6862a138c7d2f38b7ff00c0df833304790db56c4
Mioszek <228871@edu.p.lodz.pl>
2022-11-15 23:43:31 +0100

    add animation

--- lib/auth/views/auth_choice.dart
+++ lib/auth/views/auth_choice.dart
@@ -4,39 +4,78 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../providers/controllers/auth_controller_provider.dart';
 
-class AuthChoice extends ConsumerWidget {
+class AuthChoice extends ConsumerStatefulWidget {
   const AuthChoice({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<AuthChoice> createState() => _AuthChoiceState();
+}
+
+class _AuthChoiceState extends ConsumerState<AuthChoice>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    // duration: const Duration(seconds: 2),
+    vsync: this,
+  );
+  // late final Animation<Offset> _offsetAnimation = Tween<Offset>(
+  //   begin: const Offset(1.5, 0.0),
+  //   end: const Offset(0.0, 0.0),
+  // ).animate(CurvedAnimation(
+  //   parent: _controller,
+  //   curve: Curves.bounceIn,
+  // ));
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              child: Text(
-                'log in',
+        child: TweenAnimationBuilder(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                child: Text(
+                  'log in',
+                  style: authChoiceTextStyle,
+                ),
+                onPressed: () {
+                  ref.read(authControllerProvider.notifier).showLogin();
+                },
+              ),
+              Text(
+                '/',
                 style: authChoiceTextStyle,
               ),
-              onPressed: () {
-                ref.read(authControllerProvider.notifier).showLogin();
-              },
-            ),
-            Text(
-              '/',
-              style: authChoiceTextStyle,
-            ),
-            TextButton(
-              child: Text(
-                'sign up',
-                style: authChoiceTextStyle,
+              TextButton(
+                child: Text(
+                  'sign up',
+                  style: authChoiceTextStyle,
+                ),
+                onPressed: () {
+                  ref.read(authControllerProvider.notifier).showSignUp();
+                },
               ),
-              onPressed: () {
-                ref.read(authControllerProvider.notifier).showSignUp();
-              },
-            ),
-          ],
+            ],
+          ),
+          tween: Tween<double>(
+            begin: 0,
+            end: 1,
+          ),
+          duration: Duration(seconds: 2),
+          builder: (context, double val, child) {
+            return Opacity(
+              opacity: val,
+              child: Padding(
+                padding: EdgeInsets.only(top: val * 10),
+                child: child,
+              ),
+            );
+          },
         ),
       ),
     );
