07979f17f17dff3699a1cd04594c9d24e735ff6c
Mioszek <228871@edu.p.lodz.pl>
2023-06-18 02:06:50 +0200

    add popup to markers and open google maps button

--- lib/views/carbonless_map/carbonless_map_view.dart
+++ lib/views/carbonless_map/carbonless_map_view.dart
@@ -1,15 +1,13 @@
 import 'package:carbonless/main.dart';
 import 'package:carbonless/models/location_model.dart';
 import 'package:carbonless/models/partner_model.dart';
-import 'package:carbonless/providers/controllers/geolocation/geolocation_controller_provider.dart';
 import 'package:carbonless/providers/controllers/partners/partners_controller_provider.dart';
 import 'package:carbonless/views/carbonless_map/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:geocoder/geocoder.dart';
 
 class CarbonlessMapView extends ConsumerStatefulWidget {
   const CarbonlessMapView({Key? key}) : super(key: key);
@@ -23,9 +21,19 @@ class _CarbonlessMapViewState extends ConsumerState<CarbonlessMapView> {
   double lat = -0.09;
   LatLng point = LatLng(49.5, -0.09);
   var location = [];
+  final PopupController _popupLayerController = PopupController();
+
   @override
   Widget build(BuildContext context) {
     List<Partner> partners = ref.watch(partnersNotifier);
+    List<PartnerMarker> markers = [
+      for (Partner partner in partners)
+        for (Location location in partner.locations)
+          PartnerMarker(
+            partner: partner,
+            location: LatLng(location.latitude, location.longitude),
+          ),
+    ];
     return Container(
       child: Stack(
         children: [
@@ -42,11 +50,15 @@ class _CarbonlessMapViewState extends ConsumerState<CarbonlessMapView> {
               // print(
               //     "${location.first.countryName} - ${location.first.featureName}");
               // },
+              onTap: (_, __) {
+                _popupLayerController.hideAllPopups();
+              },
               center: LatLng(49.5, -0.09),
               zoom: 5.0,
             ),
-            layers: [
-              TileLayerOptions(
+            children: [
+              TileLayerWidget(
+                  options: TileLayerOptions(
                 urlTemplate:
                     "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
                 subdomains: ['a', 'b', 'c'],
@@ -54,16 +66,21 @@ class _CarbonlessMapViewState extends ConsumerState<CarbonlessMapView> {
                     ? null
                     : darkModeTileBuilder,
                 // backgroundColor: Colors.black54,
-              ),
-              MarkerLayerOptions(
-                markers: [
-                  for (Partner partner in partners)
-                    for (Location location in partner.locations)
-                      partnerMarker(
-                        partner,
-                        LatLng(location.latitude, location.longitude),
-                      ),
-                ],
+              )),
+              PopupMarkerLayerWidget(
+                options: PopupMarkerLayerOptions(
+                  markerRotate: false,
+                  markers: markers,
+                  popupController: _popupLayerController,
+                  markerTapBehavior: MarkerTapBehavior.togglePopupAndHideRest(),
+                  popupSnap: PopupSnap.markerTop,
+                  popupBuilder: (BuildContext context, Marker marker) {
+                    return MarkerPopup(marker: marker as PartnerMarker);
+                  },
+                  onPopupEvent: (dynamic popupEvent, List<Marker> marker) {
+                    print('popupevent');
+                  },
+                ),
               ),
             ],
           ),
--- lib/views/carbonless_map/widgets.dart
+++ lib/views/carbonless_map/widgets.dart
@@ -1,8 +1,28 @@
 import 'package:carbonless/models/partner_model.dart';
+import 'package:carbonless/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:latlong2/latlong.dart';
+
+import 'map_utils.dart';
+
+class PartnerMarker extends Marker {
+  final Partner partner;
+  final LatLng location;
+  PartnerMarker({required this.partner, required this.location})
+      : super(
+          point: location,
+          width: 80,
+          height: 80,
+          builder: (context) => SizedBox(
+            child: Icon(
+              Icons.location_on,
+              color: primaryColor,
+            ),
+          ),
+        );
+}
 
 Marker partnerMarker(Partner partner, LatLng point) {
   return Marker(
@@ -10,10 +30,49 @@ Marker partnerMarker(Partner partner, LatLng point) {
     height: 80,
     point: point,
     builder: (context) => Container(
+      width: 80,
+      height: 80,
       child: Icon(
         Icons.location_on,
         color: Colors.red,
       ),
     ),
+    anchorPos: AnchorPos.align(AnchorAlign.top),
+    // rotateAlignment: AnchorAlign.top,
   );
+}
+
+class MarkerPopup extends StatefulWidget {
+  PartnerMarker marker;
+  MarkerPopup({Key? key, required this.marker}) : super(key: key);
+
+  @override
+  State<MarkerPopup> createState() => _MarkerPopupState();
+}
+
+class _MarkerPopupState extends State<MarkerPopup> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(widget.marker.partner.name),
+            ElevatedButton(
+              onPressed: () async {
+                await MapUtils.openMap(widget.marker.location.latitude,
+                    widget.marker.location.longitude);
+              },
+              child: Text("Navigate to"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
