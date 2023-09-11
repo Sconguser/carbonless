e619afe4e29511f4e174a94a0897e756becfa0a1
Mioszek <228871@edu.p.lodz.pl>
2023-09-11 22:25:58 +0200

    ref

--- lib/views/scanner/widgets.dart
+++ lib/views/scanner/widgets.dart
@@ -1,6 +1,3 @@
-import 'package:carbonless/providers/controllers/qr_scanner_controller_provider.dart';
-import 'package:carbonless/providers/controllers/travel_session_controller_provider/travel_session_controller_provider.dart';
-import 'package:carbonless/providers/states/travel_session_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -17,70 +14,9 @@ class QrSendConfirmationWindow extends ConsumerWidget {
     return Container(
       child: viewState is QrScannerStateBusy
           ? CircularProgressIndicator()
-          : getQrSendConfirmationWindow(ref),
-    );
-  }
-}
-
-Widget getQrSendConfirmationWindow(WidgetRef ref) {
-  TravelSessionState travelSessionState =
-      ref.read(travelSessionControllerProvider);
-  if (travelSessionState == const TravelSessionOpen()) {
-    return const SessionStarted();
-  } else {
-    return PointsEarned(points: 200);
-  }
-}
-
-class PointsEarned extends ConsumerWidget {
-  int points;
-  PointsEarned({Key? key, required this.points}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'You earned $points points!',
-            style: logoTextStyle,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              ref.read(qrScannerControllerProvider.notifier).reset();
-            },
-            child: Text('Okay'),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class SessionStarted extends ConsumerWidget {
-  const SessionStarted({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Center(
-      child: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Session started!',
-              style: logoTextStyle,
+          : Center(
+              child: Text('meh'),
             ),
-            ElevatedButton(
-              onPressed: () {
-                ref.read(qrScannerControllerProvider.notifier).reset();
-              },
-              child: Text('Cool'),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
