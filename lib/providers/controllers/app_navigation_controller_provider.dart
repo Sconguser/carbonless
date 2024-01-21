ecc402e70c46964fc5866dc1494564e62f82d6a8
Mioszek <228871@edu.p.lodz.pl>
2024-01-21 13:19:47 +0100

    fix appbar actions provider

--- lib/providers/controllers/app_navigation_controller_provider.dart
+++ lib/providers/controllers/app_navigation_controller_provider.dart
@@ -25,18 +25,15 @@ class AppNavigationController extends StateNotifier<AppNavigationState> {
   }
 
   void showMain(AppNavigationState state) {
-    ref.read(appbarActionsControllerNotifier.notifier).setActionsForTypeView();
     this.state = state;
   }
 
   void showAuxiliary(AppNavigationState state) {
-    // ref.read(appbarActionsControllerNotifier.notifier).setActionsForTypeView();
     this.state = state;
     ref.read(bottomNavIndexProvider.notifier).state = -1;
   }
 
   void reset() {
-    ref.read(appbarActionsControllerNotifier.notifier).clearActions();
     state = getDefaultView();
     ref.read(bottomNavIndexProvider.notifier).state = 0;
   }
--- lib/providers/controllers/view_behavior_controllers/appbar/appbar_actions_controller_provider.dart
+++ lib/providers/controllers/view_behavior_controllers/appbar/appbar_actions_controller_provider.dart
@@ -1,37 +1,50 @@
+import 'package:carbonless_free/providers/controllers/app_navigation_controller_provider.dart';
+import 'package:carbonless_free/providers/states/navigation/exchanges/offers_navigation_state.dart';
+import 'package:carbonless_free/views/exchange/offers/create/create_view.dart';
+import 'package:carbonless_free/views/exchange/offers/offer_list/exchanges_list/exchange_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../../main.dart';
+import '../../../states/navigation/app_navigation_state.dart';
+
 class AppbarActionsController extends StateNotifier<List<Widget>> {
-  AppbarActionsController(List<Widget> actions, this.ref) : super(actions);
+  AppbarActionsController(List<Widget> actions, this.ref) : super(actions) {
+    ref.watch(appNavigationControllerProvider.notifier).addListener((state) {
+      setActions(getActionsForState(ref, state));
+    });
+  }
   final Ref ref;
 
   void setActions(List<Widget> actions) {
     state = actions;
   }
 
-  void setActionsForTypeView() {}
-
   void clearActions() {
     state = [];
   }
 }
 
-// List<Widget> getActionsForOffersView(Ref ref) {
-//   if (ref.read(exchangeNavigationControllerProvider) == const CreateOffer()) {
-//     return [];
-//   }
-//   return [
-//     ElevatedButton(
-//       child: Text(ref.read(messagesProvider).button.add_offer),
-//       onPressed: () {
-//         ref.read(exchangeNavigationControllerProvider.notifier).createOffer();
-//       },
-//     )
-//   ];
-// }
+List<Widget> getActionsForState(Ref ref, AppNavigationState state) {
+  if (state is AppNavigationAuxiliary) {
+    return [];
+  }
+  if (state is OfferListView) {
+    return [
+      ElevatedButton(
+        child: Text(ref.read(messagesProvider).button.add_offer),
+        onPressed: () {
+          ref
+              .read(appNavigationControllerProvider.notifier)
+              .showMain(const NewOfferView());
+        },
+      )
+    ];
+  }
+  return [];
+}
 
 final appbarActionsControllerNotifier =
     StateNotifierProvider<AppbarActionsController, List<Widget>>((ref) {
-  // List<Widget>? actions = getActionsForOffersView(ref);
   return AppbarActionsController([], ref);
 });
